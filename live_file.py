2d9e654a998bda36ef51d42a7aa91a5e3fdcbf19
unknown <mutungaeric839@gmail.com>
2024-01-18 15:15:13 +0300

    updated the code to handle three streams

--- live_file.py
+++ live_file.py
@@ -1,43 +1,54 @@
 import pandas as pd 
 from googleapiclient.discovery import build
 
-def get_youtube_data(api_key, video_id):
+def get_youtube_data(api_key, *video_ids):
     youtube = build('youtube', 'v3', developerKey=api_key)
-
-    # Get video details
-    video_response = youtube.videos().list(part='snippet,liveStreamingDetails', id=video_id).execute()
     
-    print(video_response)
+    all_data = []
     
-    # Check if 'items' is present in the response and not empty
-    if 'items' in video_response and video_response['items']:
-        video_details = video_response['items'][0]['snippet']
-        live_details = video_response['items'][0].get('liveStreamingDetails', {})
+    for video_id in video_ids:
+        # Get video details
+        video_response = youtube.videos().list(part='snippet,liveStreamingDetails', id=video_id).execute()
+        
+        
+        print(video_response)
+        
+        # Check if 'items' is present in the response and not empty
+        if 'items' in video_response and video_response['items']:
+            video_details = video_response['items'][0]['snippet']
+            live_details = video_response['items'][0].get('liveStreamingDetails', {})
 
-        data = {
-            'Title': video_details.get('title', ''),
-            'Video URL': f'https://www.youtube.com/watch?v={video_id}',
-            'Views': live_details.get('concurrentViewers', 'Live Stream'),
-            'Length (seconds)': live_details.get('actualStartTime', ''),
-            'Author': video_details.get('channelTitle', ''),
-            'Published Date': video_details.get('publishedAt', ''),
-            'Description': video_details.get('description', ''),
-            'Thumbnail URL': video_details['thumbnails']['default']['url'] if 'thumbnails' in video_details else '',
-        }
+            data = {
+                'Title': video_details.get('title', ''),
+                'Video URL': f'https://www.youtube.com/watch?v={video_id}',
+                'Views': live_details.get('concurrentViewers', 'Live Stream'),
+                'Length (seconds)': live_details.get('actualStartTime', ''),
+                'Author': video_details.get('channelTitle', ''),
+                'Published Date': video_details.get('publishedAt', ''),
+                'Description': video_details.get('description', ''),
+                'Thumbnail URL': video_details['thumbnails']['default']['url'] if 'thumbnails' in video_details else '',
+            }
 
-        return data
-    else:
-        print(f"Error: No video details found for video ID {video_id}")
-        return None
+            all_data.append(data)
+        else:
+            print(f"Error: No video details found for video ID {video_id}")
+            
+    return all_data
 
 def main():
-    api_key = 'Put your api key'
-    video_id = 'cq3jNiquchw'  # Replace with your actual video ID
-
-    video_data = get_youtube_data(api_key, video_id)
+    api_key = 'AIzaSyA70PJAcyWR0UbGUq98E8fpD_-ysHSuLKo'
     
-    if video_data:
-        df = pd.DataFrame([video_data])
+    ktn_home = "3HKRlDRWdbc"
+    ktn_news = "OdIxeE2eWSg"
+    citizen_news = "cq3jNiquchw"
+    
+    video_ids = [citizen_news, ktn_news , ktn_home]  # Replace with your actual video IDs
+
+
+    videos_data = get_youtube_data(api_key, *video_ids)
+    
+    if videos_data:
+        df = pd.DataFrame(videos_data)
         
         # Export the DataFrame to an Excel file
         excel_filename = 'output/youtube_live_data.xlsx'
